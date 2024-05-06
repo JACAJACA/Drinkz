e65f95712f02b2b14c53be7ee3d04beb96b64377
Sebastian Mazgaj <rementai1@gmail.com>
2024-05-06 18:52:42 +0200

    Dodanie HomeScreen.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function HomeScreen() {
+    return (
+        <View className="flex-1 flex-col items-center justify-center">
+            <Text classname="text-4xl">HomeScreen</Text>
+        </View>
+    )
+}
